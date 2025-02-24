d2028550c4ddb739197c1b809dff54a1567a3a00
JesusEnjamio <jenjamiob@gmail.com>
2025-02-24 20:42:22 +0100

    testListarComputador is now working

--- src/test/java/dev/jesus/computers_shop/tienda/TiendaTest.java
+++ src/test/java/dev/jesus/computers_shop/tienda/TiendaTest.java
@@ -4,6 +4,8 @@ import dev.jesus.computers_shop.computador.Computador;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 public class TiendaTest {
     @Test
     public void testAgregarComputador() {
@@ -32,5 +34,20 @@ public class TiendaTest {
         tienda.agregarComputador(computador);
         assertNotNull(tienda.buscarComputador("Dell"));
         assertNull(tienda.buscarComputador("HP"));
+    }
+
+    @Test
+    public void testListarComputadores() {
+        Tienda tienda = new Tienda("TechStore", "John Doe", "ID123456789");
+        Computador computador1 = new Computador("Dell", 16, "Intel i7", "Windows 10", 1200.0);
+        Computador computador2 = new Computador("HP", 8, "AMD Ryzen 5", "Linux", 800.0);
+
+        tienda.agregarComputador(computador1);
+        tienda.agregarComputador(computador2);
+
+        List<Computador> lista = tienda.listarComputadores();
+        assertEquals(2, lista.size());
+        assertTrue(lista.contains(computador1));
+        assertTrue(lista.contains(computador2));
     }
 }
